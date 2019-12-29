39bb2e2556b04cba2584ae872cba36308e579763
gervisbermudez <gervisbermudez@outlook.com>
2019-12-29 12:39:06 -0300

    3 files modified, 1 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+//Core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+//Components
 import { AppComponent } from "./app.component";
 import { AppMenu } from "./menu-component/menu.component";
 import { AppSearch } from "./search-component/search.component";
 import { AppFixedBtn } from "./fixed-btn-component/fixed.btn.component";
 import { AppContactList } from "./contact-list-component/contact.list.component";
 
+//Storage
+import { StorageServiceModule } from 'ngx-webstorage-service';
+
+//Services
+import {LocalStorageService} from './app/Services/contacts.service';
+
 @NgModule({
   declarations: [AppComponent, AppMenu, AppSearch, AppFixedBtn, AppContactList],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, StorageServiceModule],
+  providers: [LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/services/contacts.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+
+// key that is used to access the data in local storageconst STORAGE_KEY = 'local_todolist';
+STORAGE_KEY = 'local_todolist';
+
+@Injectable()
+export class LocalStorageService {
+     anotherTodolist = [];
+     constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
+     public storeOnLocalStorage(taskTitle: string): void {
+          
+          // get array of tasks from local storage
+          const currentTodoList = this.storage.get(STORAGE_KEY) || [];
+          // push new task to array
+          currentTodoList.push({
+              title: taskTitle,
+              isChecked: false 
+          });
+          // insert updated array to local storage
+          this.storage.set(STORAGE_KEY, currentTodoList);
+          console.log(this.storage.get(STORAGE_KEY) || 'LocaL storage is empty');
+     }
+}
