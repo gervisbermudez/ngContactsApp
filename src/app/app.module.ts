1890d6bee548109d180642e81ff30774333d5e3b
Gervis Bermudez <gervisbermudez15@gmail.com>
2020-01-04 14:53:03 -0300

    Organize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { FormsModule } from "@angular/forms";
 
 //Components
 import { AppComponent } from "./app.component";
-import { AppMenu } from "./menu-component/menu.component";
-import { AppSearch } from "./search-component/search.component";
-import { AppFixedBtn } from "./fixed-btn-component/fixed.btn.component";
-import { AppContactList } from "./contact-list-component/contact.list.component";
+import { AppMenu } from "./components/menu-component/menu.component";
+import { AppSearch } from "./components/search-component/search.component";
+import { AppFixedBtn } from "./components/fixed-btn-component/fixed.btn.component";
+import { AppContactList } from "./components/contact-list-component/contact.list.component";
 
 //Storage
 import { StorageServiceModule } from "ngx-webstorage-service";
